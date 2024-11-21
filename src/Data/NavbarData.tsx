2a996ec3772f56f24391d75fb1d546cc67d2cd13
Can <kccecen@gmail.com>
2024-11-22 01:39:52 +0300

    deneme3

--- src/Data/NavbarData.tsx
+++ src/Data/NavbarData.tsx
@@ -2,8 +2,8 @@ import React from "react";
 
 const NavbarData = [
   { id: 1, name: "Store", road: "/ShoppingCart" },
-  { id: 2, name: "About", road: "/about" },
-  { id: 3, name: "Home", road: "/home" },
+  { id: 2, name: "About", road: "/ShoppingCart" },
+  { id: 3, name: "Home", road: "/ShoppingCart" },
 ];
 
 export default NavbarData;
